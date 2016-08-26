f58c6f1735ce1afa4fb811365b570b95e65cc562
dennycheng <chen2368@umn.edu>
2016-08-26 10:16:19 -0500

    Worked on delete function

--- server/public/scripts/controllers/peopleController.js
+++ server/public/scripts/controllers/peopleController.js
@@ -22,27 +22,8 @@ $scope.addNumber = function(person){
   $scope.totalSalary += parseInt(person.yearSalary);
 }
 
-
-// $('#employeeinfo').on('submit', function(event) {
-//   event.preventDefault();
-//
-//   var values = {};
-//   var fields = $('#employeeinfo').serializeArray();
-//   //Converts input into array
-//
-//   fields.forEach(function(element) {
-//     values[element.name] = element.value;
-//   });
-//   //Convers array into objects
-//
-//   totalMSalary += parseFloat(values.salaryY/12);
-//   //Takes collective salary figure then convets it into a monhtly figure
-//   //values.salary is a object method
-//
-//   $('#employeeinfo').find('input[type=text]').val('');
-//   //Resets input fields
-//   appendDom(values);
-// });
-
+$scope.deletePerson = function(){
+  
+}
 
 }]);
